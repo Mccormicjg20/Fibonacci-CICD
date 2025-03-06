11519fc9ccde3beaaeb66ea0688a118cdcc0fea7
Mccormicjg20 <mccormicjg20@uww.edu>
2025-03-05 18:46:25 -0600

    Its a change to 9

--- fibonacci.py
+++ fibonacci.py
@@ -12,7 +12,7 @@ def calculate_fibonacci(n):
 	return fib
 		
 def main():
-	n = 5  # Calculate first 10 Fibonacci numbers
+	n = 9  # Calculate first 10 Fibonacci numbers
 	result = calculate_fibonacci(n)
 	print(f"First {n} Fibonacci numbers: {result}")
 		
